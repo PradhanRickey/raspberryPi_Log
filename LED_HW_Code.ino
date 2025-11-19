5ac8c1a47fb06d2e047ee770dc08d7b8efd077e1
Rickey Pradhan <rickeypradhan22@gmail.com>
2025-11-18 20:43:02 -0800

    connected 3 leds and made them blink

---
+++ LED_HW_Code.ino
@@ -0,0 +1,117 @@
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(13,OUTPUT);
+
+  digitalWrite(13,HIGH);
+  delay(500);
+  digitalWrite(13,LOW);
+  delay(500);
+  digitalWrite(13,HIGH);
+  delay(500);
+  digitalWrite(13,LOW);
+  delay(500);
+  digitalWrite(13,HIGH);
+  delay(500);
+  digitalWrite(13,LOW);
+  delay(500);
+  digitalWrite(13,HIGH);
+  delay(500);
+  digitalWrite(13,LOW);
+  delay(500);
+  digitalWrite(13,HIGH);
+  delay(500);
+  digitalWrite(13,LOW);
+  delay(500);
+
+  pinMode(11,OUTPUT);
+
+  digitalWrite(11,HIGH);
+  delay(500);
+  digitalWrite(11,LOW);
+  delay(500);
+  digitalWrite(11,HIGH);
+  delay(500);
+  digitalWrite(11,LOW);
+  delay(500);
+  digitalWrite(11,HIGH);
+  delay(500);
+  digitalWrite(11,LOW);
+  delay(500);
+  digitalWrite(11,HIGH);
+  delay(500);
+  digitalWrite(11,LOW);
+  delay(500);
+  digitalWrite(11,HIGH);
+  delay(500);
+  digitalWrite(11,LOW);
+  delay(500);
+  digitalWrite(11,HIGH);
+  delay(500);
+  digitalWrite(11,LOW);
+  delay(500);
+  digitalWrite(11,HIGH);
+  delay(500);
+  digitalWrite(11,LOW);
+  delay(500);
+  digitalWrite(11,HIGH);
+  delay(500);
+  digitalWrite(11,LOW);
+  delay(500);
+  digitalWrite(11,HIGH);
+  delay(500);
+  digitalWrite(11,LOW);
+  delay(500);
+  digitalWrite(11,HIGH);
+  delay(500);
+  digitalWrite(11,LOW);
+  delay(500);
+
+  pinMode(8,OUTPUT);
+
+  digitalWrite(8,HIGH);
+  delay(500);
+  digitalWrite(8,LOW);
+  delay(500);
+  digitalWrite(8,HIGH);
+  delay(500);
+  digitalWrite(8,LOW);
+  delay(500);
+  digitalWrite(8,HIGH);
+  delay(500);
+  digitalWrite(8,LOW);
+  delay(500);
+  digitalWrite(8,HIGH);
+  delay(500);
+  digitalWrite(8,LOW);
+  delay(500);
+  digitalWrite(8,HIGH);
+  delay(500);
+  digitalWrite(8,LOW);
+  delay(500);
+  digitalWrite(8,HIGH);
+  delay(500);
+  digitalWrite(8,LOW);
+  delay(500);
+  digitalWrite(8,HIGH);
+  delay(500);
+  digitalWrite(8,LOW);
+  delay(500);
+  digitalWrite(8,HIGH);
+  delay(500);
+  digitalWrite(8,LOW);
+  delay(500);
+  digitalWrite(8,HIGH);
+  delay(500);
+  digitalWrite(8,LOW);
+  delay(500);
+  digitalWrite(8,HIGH);
+  delay(500);
+  digitalWrite(8,LOW);
+  delay(500);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
